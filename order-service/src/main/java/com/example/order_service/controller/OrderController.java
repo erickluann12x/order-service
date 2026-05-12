a56ddb06cd1941f104938a266d6cc060914f4bd1
ErickLuan <erickluan12x@gmail.com>
2026-05-12 20:56:20 -0300

    create controller

---
+++ order-service/src/main/java/com/example/order_service/controller/OrderController.java
@@ -0,0 +1,11 @@
+package com.example.order_service.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/create")
+public class OrderController {
+
+}
