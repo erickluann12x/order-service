0160815c59d46868ffb9f1b0f07ca1aff2405b7b
ErickLuan <erickluan12x@gmail.com>
2026-05-13 00:30:58 -0300

    Config Controller Get e Post

--- order-service/src/main/java/com/example/order_service/controller/OrderController.java
+++ order-service/src/main/java/com/example/order_service/controller/OrderController.java
@@ -1,11 +1,35 @@
 package com.example.order_service.controller;
 
+import com.example.order_service.dto.OrderRequestDTO;
+import com.example.order_service.entity.Order;
+import com.example.order_service.service.OrderService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/create")
-public class OrderController {
+import java.util.List;
 
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+    @Autowired
+    private OrderService orderService;
+
+    @PostMapping
+    public ResponseEntity<Order> createOrder(@Valid @RequestBody OrderRequestDTO orderRequestDTO){
+        Order orderResponseDTO = orderService.createOrder(orderRequestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(orderResponseDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Order>> findAllOrders(){
+        List<Order> orderList = orderService.IfindAllOrders();
+        return ResponseEntity.ok(orderList);
+    }
 }
