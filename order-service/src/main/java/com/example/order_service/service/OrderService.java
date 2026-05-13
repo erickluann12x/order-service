a37e8e555333ec776a4a64d1d71b708df66ec72b
ErickLuan <erickluan12x@gmail.com>
2026-05-13 00:29:13 -0300

    Update Service CreateOrder

--- order-service/src/main/java/com/example/order_service/service/OrderService.java
+++ order-service/src/main/java/com/example/order_service/service/OrderService.java
@@ -7,6 +7,9 @@ import com.example.order_service.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Service
 public class OrderService {
     @Autowired
@@ -19,7 +22,11 @@ public class OrderService {
         order.setEmail(orderRequestDTO.email());
         order.setTotalAmount(orderRequestDTO.totalAmount());
         order.setStatus(StatusOrder.PENDING);
-        order.getCreatedAt();
+        order.setCreatedAt(LocalDateTime.now());
         return orderRepository.save(order);
+    }
+
+    public List<Order> IfindAllOrders(){
+        return orderRepository.findAll();
     }
 }
