6239fd132147ae2a5899be47d1500bfb9f894991
ErickLuan <erickluan12x@gmail.com>
2026-05-13 00:30:05 -0300

    Request @notnull/notblank

--- order-service/src/main/java/com/example/order_service/dto/OrderRequestDTO.java
+++ order-service/src/main/java/com/example/order_service/dto/OrderRequestDTO.java
@@ -2,8 +2,9 @@ package com.example.order_service.dto;
 
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record OrderRequestDTO(@NotBlank(message = "O Nome Obrigatório") String name,
                               @NotBlank(message = "O Email é Obrigatório") String email,
-                              @NotBlank(message = "Valor Total Obrigatório") Integer totalAmount) {
+                              @NotNull(message = "Valor Total Obrigatório") Integer totalAmount) {
 }
