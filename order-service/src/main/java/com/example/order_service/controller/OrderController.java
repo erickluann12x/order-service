8a90bbc2e5c1fe8ec0cd918d95ab703726dcdda9
ErickLuan <erickluan12x@gmail.com>
2026-05-19 23:00:14 -0300

    Create Controller FindOrderById

--- order-service/src/main/java/com/example/order_service/controller/OrderController.java
+++ order-service/src/main/java/com/example/order_service/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.example.order_service.controller;
 
 import com.example.order_service.dto.OrderRequestDTO;
+import com.example.order_service.dto.OrderResponseDTO;
 import com.example.order_service.entity.Order;
 import com.example.order_service.service.OrderService;
 import jakarta.validation.Valid;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,5 +33,10 @@ public class OrderController {
     public ResponseEntity<List<Order>> findAllOrders(){
         List<Order> orderList = orderService.IfindAllOrders();
         return ResponseEntity.ok(orderList);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<OrderResponseDTO> IfindOrderById(@PathVariable Long id ){
+        OrderResponseDTO response = orderService.IfindOrderById(id);
+        return ResponseEntity.ok(response);
     }
 }
