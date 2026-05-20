9e845cf805400dd3c4ca5e3d64d946f8a5094f56
ErickLuan <erickluan12x@gmail.com>
2026-05-19 23:01:02 -0300

    Update Repo

--- order-service/src/main/java/com/example/order_service/repository/OrderRepository.java
+++ order-service/src/main/java/com/example/order_service/repository/OrderRepository.java
@@ -4,7 +4,9 @@ import com.example.order_service.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface OrderRepository extends JpaRepository<Order, Long > {
+import java.util.List;
+import java.util.Optional;
 
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
 }
